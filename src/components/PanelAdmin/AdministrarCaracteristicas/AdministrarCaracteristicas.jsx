0d27001950cba943796a177526e093301f063ff1
Andrea Latuff <andrealatuff@MacBook-Air-de-Andrea.local>
2023-08-24 13:22:17 -0500

    Ayude a Joaco a visualizar los iconos en la lista relacionada a caracteristicas... IMPLEMENTE REACT SELECT, por lo que hay que instalar para poder correr el proyecto npm install react-select. Inicie a estilizar el login y logout pero termino de hacerlo en mi rama

--- src/components/PanelAdmin/AdministrarCaracteristicas/AdministrarCaracteristicas.jsx
+++ src/components/PanelAdmin/AdministrarCaracteristicas/AdministrarCaracteristicas.jsx
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
-import { FaAviato, FaRegSquare, FaToriiGate } from 'react-icons/fa';
-import styles from './administrarCaracteristicas.module.css';
+import React, { useState } from "react";
+import Select from "react-select";
+import { FaAviato, FaRegSquare, FaToriiGate } from "react-icons/fa";
+import styles from "./administrarCaracteristicas.module.css";
+
+const iconOptions = [
+  { value: "FaAviato", label: <FaAviato size={20} /> },
+  { value: "FaRegSquare", label: <FaRegSquare size={20} /> },
+  { value: "FaToriiGate", label: <FaToriiGate size={20} /> },
+];
 
 const AdministrarCaracteristicas = () => {
   const [caracteristicas, setCaracteristicas] = useState([]);
-  const [nuevaCaracteristica, setNuevaCaracteristica] = useState('');
-  const [nuevoIcono, setNuevoIcono] = useState('');
+  const [nuevaCaracteristica, setNuevaCaracteristica] = useState("");
+  const [nuevoIcono, setNuevoIcono] = useState("");
   const [caracteristicaEditando, setCaracteristicaEditando] = useState(null);
 
-  const iconosDisponibles = [ FaAviato, FaRegSquare, FaToriiGate];
+  const iconosDisponibles = {
+    FaAviato: <FaAviato size={20} />,
+    FaRegSquare: <FaRegSquare size={20} />,
+    FaToriiGate: <FaToriiGate size={20} />,
+  };
 
   const agregarCaracteristica = () => {
     if (nuevaCaracteristica && nuevoIcono) {
-      setCaracteristicas([...caracteristicas, { nombre: nuevaCaracteristica, icono: nuevoIcono }]);
-      setNuevaCaracteristica('');
-      setNuevoIcono('');
+      setCaracteristicas([
+        ...caracteristicas,
+        { nombre: nuevaCaracteristica, icono: iconosDisponibles[nuevoIcono] },
+      ]);
+      setNuevaCaracteristica("");
+      setNuevoIcono("");
     }
   };
 
@@ -34,16 +48,21 @@ const AdministrarCaracteristicas = () => {
   };
 
   const handleDelete = (indice) => {
-    const confirmDelete = window.confirm('¿Estás seguro de que deseas eliminar este producto?');
+    const confirmDelete = window.confirm(
+      "¿Estás seguro de que deseas eliminar este producto?"
+    );
     if (confirmDelete) {
-    const nuevasCaracteristicas = caracteristicas.filter((_, i) => i !== indice);
-    setCaracteristicas(nuevasCaracteristicas);
-  }
-};
+      const nuevasCaracteristicas = caracteristicas.filter(
+        (_, i) => i !== indice
+      );
+      setCaracteristicas(nuevasCaracteristicas);
+    }
+  };
 
-return (
+  return (
     <div className={styles.administrarCaracteristicas}>
       <h2>Administrar Características</h2>
+
       <div className={styles.agregarCaracteristica}>
         <input
           type="text"
@@ -51,62 +70,69 @@ return (
           value={nuevaCaracteristica}
           onChange={(e) => setNuevaCaracteristica(e.target.value)}
         />
-        <select
-          value={nuevoIcono}
-          onChange={(e) => setNuevoIcono(e.target.value)}
-        >
-          <option value="">Selecciona un icono</option>
-          {iconosDisponibles.map((Icon, index) => (
-            <option key={index} value={Icon}>
-              {<Icon size={20} />} {/* Mostrar el icono en el selector */}
-            </option>
-          ))}
-        </select>
+        <Select
+          value={{ value: nuevoIcono, label: iconosDisponibles[nuevoIcono] }}
+          options={iconOptions}
+          onChange={(selectedOption) => setNuevoIcono(selectedOption.value)}
+          placeholder="Selecciona un icono"
+        />
         <button onClick={agregarCaracteristica}>Añadir Nueva</button>
       </div>
+
       <ul className={styles.listaCaracteristicas}>
         {caracteristicas.map((caracteristica, indice) => (
           <li key={indice}>
-          {caracteristicaEditando?.indice === indice ? (
-            <>
-              <input
-                type="text"
-                value={caracteristicaEditando.nombre}
-                onChange={(e) =>
-                  setCaracteristicaEditando({
-                    ...caracteristicaEditando,
-                    nombre: e.target.value,
-                  })
-                }
-              />
-              <input
-                type="text"
-                value={caracteristicaEditando.icono}
-                onChange={(e) =>
-                  setCaracteristicaEditando({
-                    ...caracteristicaEditando,
-                    icono: e.target.value,
-                  })
-                }
-              />
-              <button onClick={() => saveEdit(indice, caracteristicaEditando.nombre, caracteristicaEditando.icono)}>
-                Guardar
-              </button>
-              <button onClick={cancelEdit}>Cancelar</button>
-            </>
-          ) : (
-            <>
-              <span>{caracteristica.nombre}</span>
-              <span>{caracteristica.icono}</span>
-            </>
-          )}
-          {caracteristicaEditando?.indice !== indice && (
-            <>
-              <button onClick={() => handleEdit(indice, caracteristica.nombre, caracteristica.icono)}>Editar</button>
-              <button onClick={() => handleDelete(indice)}>Eliminar</button>
-            </>
-          )}
-        </li>
+            {caracteristicaEditando?.indice === indice ? (
+              <>
+                <input
+                  type="text"
+                  value={caracteristicaEditando.nombre}
+                  onChange={(e) =>
+                    setCaracteristicaEditando({
+                      ...caracteristicaEditando,
+                      nombre: e.target.value,
+                    })
+                  }
+                />
+
+                {caracteristica.icono}
+
+                <button
+                  onClick={() =>
+                    saveEdit(
+                      indice,
+                      caracteristicaEditando.nombre,
+                      caracteristicaEditando.icono
+                    )
+                  }
+                >
+                  Guardar
+                </button>
+                <button onClick={cancelEdit}>Cancelar</button>
+              </>
+            ) : (
+              <>
+                <span>{caracteristica.nombre}</span>
+                {caracteristica.icono}
+              </>
+            )}
+            {caracteristicaEditando?.indice !== indice && (
+              <>
+                <button
+                  onClick={() =>
+                    handleEdit(
+                      indice,
+                      caracteristica.nombre,
+                      caracteristica.icono
+                    )
+                  }
+                >
+                  Editar
+                </button>
+                <button onClick={() => handleDelete(indice)}>Eliminar</button>
+              </>
+            )}
+          </li>
         ))}
       </ul>
     </div>
