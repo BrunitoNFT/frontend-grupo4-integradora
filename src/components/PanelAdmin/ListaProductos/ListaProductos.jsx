472617c08cdb715bb4420eba13251b505e5e8b2b
PekeLC <joaquin.lc.castillo@gmail.com>
2023-08-29 17:28:13 -0300

    ListaProductos ready

--- src/components/PanelAdmin/ListaProductos/ListaProductos.jsx
+++ src/components/PanelAdmin/ListaProductos/ListaProductos.jsx
@@ -6,31 +6,25 @@ const ListaProductos = ({  onEditProduct }) => {
   const [productos, setProductos] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/data.json'); // Cambia la ruta según la ubicación de data.json en tu proyecto
-        const data = await response.json();
-        setProductos(data);
-      } catch (error) {
-        console.error('Error al obtener los datos de productos:', error);
-      }
-    };
-
-    fetchData();
+    fetch("http://18.118.140.140/product")
+      .then((response) => response.json()) 
+      .then((data) => {
+        setProductos(data); 
+      })
+      .catch((error) => console.log("Error fetching data:", error));
   }, []);
 
   const handleDelete = (id) => {
     const confirmDelete = window.confirm('¿Estás seguro de que deseas eliminar este producto?');
     if (confirmDelete) {
-      // Realizar llamada a la API o base de datos para eliminar el producto
-      fetch('https://jsonplaceholder.typicode.com/posts/{id}', {
-        method: 'DELETE', // Utilizar el método HTTP DELETE
+      fetch(`http://18.118.140.140/product/${id}`, {
+        method: 'DELETE',
       })
         .then(response => {
           if (response.ok) {
-            // Actualizar el estado local para reflejar el producto eliminado
             const updatedProductos = productos.filter(producto => producto.id !== id);
             setProductos(updatedProductos);
+            console.log('Producto eliminado exitosamente');
           } else {
             console.error('Error al eliminar el producto');
           }
@@ -40,55 +34,51 @@ const ListaProductos = ({  onEditProduct }) => {
         });
     }
   };
-  
 
   const handleEdit = async (id) => {
     try {
-      // Realizar la acción para obtener los datos actuales del producto
-      const response = await fetch('/data.json');
-      const data = await response.json();
+        const response = await fetch(`http://18.118.140.140/product/${id}`);
+        const data = await response.json();
   
-      // Encontrar el producto actual en los datos basado en su ID
-      const productoActual = data.find(producto => producto.id === id);
+        const productoActual = data.find(producto => producto.id === id);
   
-      // Ejecutar la función onEditProduct para pasar los datos del producto actual a la página de edición
-      onEditProduct(productoActual);
+        onEditProduct(productoActual);
     } catch (error) {
-      console.error('Error al obtener los datos del producto:', error);
+        console.error('Error al obtener los datos del producto:', error);
     }
-  };
+};
   
 
   return (
     <div className={styles.listaProductos}>
       <h2>Lista de Productos</h2>
-      <table className={styles.table}>
+      <table>
         <thead>
           <tr>
-            <th>ID</th>
+            <th>Id</th>
             <th>Imagen</th>
             <th>Nombre</th>
-            <th>Precio</th>
-            <th>Categoria</th>
-            <th className={styles.botones}></th>
+            <th>Descripcion</th>
+            <th>Stock</th>
+            <th>Acciones</th>
           </tr>
         </thead>
         <tbody>
-          {productos.map(producto => (
+          {productos.map((producto) => (
             <tr key={producto.id}>
               <td>{producto.id}</td>
               <td>
-                <img src={producto.img} alt={`Imagen de ${producto.objeto}`} />
+                <img src={producto.urlImg} alt={`Imagen de ${producto.name}`} />
               </td>
-              <td>{producto.objeto}</td>
-              <td>{producto.precio}</td>
-              <td>{producto.categoria}</td>
+              <td>{producto.name}</td>
+              <td>{producto.description}</td>
+              <td>{producto.stock}</td>
               <td>
-                <button className={styles.button} onClick={() => handleEdit(producto.id)}>
-                  <FaEdit/>
+                <button onClick={() => handleEdit(producto.id)}>
+                  <FaEdit />
                 </button>
-                <button className={styles.button} onClick={() => handleDelete(producto.id)}>
-                  <FaTrash/>
+                <button onClick={() => handleDelete(producto.id)}>
+                  <FaTrash />
                 </button>
               </td>
             </tr>
@@ -96,7 +86,7 @@ const ListaProductos = ({  onEditProduct }) => {
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
 export default ListaProductos;
