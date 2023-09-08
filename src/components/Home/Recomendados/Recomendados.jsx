d4eaf0e2dd4a6519bf025b77cce941c511a501a9
Andrea Latuff <andrealatuff@MacBook-Air-de-Andrea.local>
2023-09-08 14:06:43 -0500

    Se crea boton para compartir en redes

--- src/components/Home/Recomendados/Recomendados.jsx
+++ src/components/Home/Recomendados/Recomendados.jsx
@@ -3,13 +3,29 @@ import { DataContext } from "../../Context/DataContext";
 import styles from "./recomendados.module.css";
 import { Link } from "react-router-dom";
 import { toast } from "sonner";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsShareFill,
+} from "react-icons/bs";
 
 const Recomendados = () => {
   const { data, cart, setCart } = useContext(DataContext);
   const itemsPerPage = 10;
-
   const [currentPage, setCurrentPage] = useState(1);
   const [randomizedData, setRandomizedData] = useState([]);
+  const [comment, setComment] = useState("");
+
+  const openSharePopup = (product) => {
+    const popup = document.getElementById(`popup${product.id}`);
+    popup.style.display = "block";
+  };
+
+  const closePopup = (product) => {
+    const popup = document.getElementById(`popup${product.id}`);
+    popup.style.display = "none";
+  };
 
   useEffect(() => {
     // Mezcla el orden de los productos aleatoriamente
@@ -26,7 +42,7 @@ const Recomendados = () => {
 
   const buyProducts = (e, product) => {
     // Agrega 'product' como parámetro
-
+    
     e.preventDefault();
 
     console.log("Comprando: ", product);
@@ -42,27 +58,24 @@ const Recomendados = () => {
     return <p>Loading...</p>;
   }
 
-
-    
   return (
-
     <div className={styles.contenedorPadre}>
       <span className={styles.title}>Lo que te recomendamos</span>
 
       <div className={styles.cardConteiner}>
-      {currentItems.map((product) => (
-          <Link
-            className={styles.card}
-            key={product.id}
-            to={"/detalle/" + product.id}
-          >
-            <div className={styles.imgContainer}>
+        {currentItems.map((product) => (
+          <div className={styles.card}>
+            <Link
+              className={styles.imgContainer}
+              key={product.id}
+              to={"/detalle/" + product.id}
+            >
               <img
                 src={product.img}
                 alt="img-product-card"
                 className={styles.img}
               />
-            </div>
+            </Link>
             <div className={styles.dataContainer}>
               <div className={styles.dataContainerChildren}>
                 <span className={styles.h3}>{product.objeto}</span>
@@ -76,35 +89,94 @@ const Recomendados = () => {
               >
                 Reservar
               </button>{" "}
+              <button
+                className={styles.buttonShare}
+                onClick={() => openSharePopup(product)}
+              >
+                <BsShareFill color="#4F709C" size={15}/>
+              </button>
+              {/* Elementos para la ventana emergente de compartir */}
+              <div className={styles.sharePopup} id={`popup${product.id}`}>
+                <img
+                  className={styles.sharePopupImg}
+                  src={product.img}
+                  alt="img-product-popup"
+                />
+                <p>{product.objeto}</p>
+                <a href={`/detalle/${product.id}`} className={styles.detailLink}>
+                    Ver mas
+                </a>
+                <input
+                  className={styles.sharePopupInput}
+                  type="text"
+                  placeholder="Escribe tu comentario"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                />
+                <div className={styles.socialLinks}>
+                  <a
+                    className={styles.socialLinksA}
+                    href={`https://www.facebook.com/share?url=${window.location.href}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <BsFacebook color="#214F55"/>
+                  </a>
+                  <a
+                    className={styles.socialLinksA}
+                    href={`https://www.instagram.com/share?url=${window.location.href}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <BsInstagram color="#214F55"/>
+                  </a>
+                  <a
+                    className={styles.socialLinksA}
+                    href={`https://twitter.com/share?url=${window.location.href}&text=${product.objeto}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <BsTwitter color="#214F55"/>
+                  </a>
+                </div>
+                <button
+                  className={styles.closeButton}
+                  onClick={() => closePopup(product)}
+                >
+                  Cerrar
+                </button>
+              </div>
             </div>
-          </Link>
+          </div>
         ))}
       </div>
       <div className={styles.pagination}>
         <button
-        className={styles.buttonPagination}
+          className={styles.buttonPagination}
           onClick={() => handlePageChange(1)}
           disabled={currentPage === 1}
         >
           {"<<"}
         </button>
         <button
-        className={styles.buttonPagination}
+          className={styles.buttonPagination}
           onClick={() => handlePageChange(currentPage - 1)}
           disabled={currentPage === 1}
         >
           {"<"}
         </button>
-        <span className={styles.numeracion}>{`${currentPage} / ${totalPages}`}</span>
+        <span
+          className={styles.numeracion}
+        >{`${currentPage} / ${totalPages}`}</span>
         <button
-        className={styles.buttonPagination}
+          className={styles.buttonPagination}
           onClick={() => handlePageChange(currentPage + 1)}
           disabled={currentPage === totalPages}
         >
           {">"}
         </button>
         <button
-        className={styles.buttonPagination}
+          className={styles.buttonPagination}
           onClick={() => handlePageChange(totalPages)}
           disabled={currentPage === totalPages}
         >
